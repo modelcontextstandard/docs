03d4a964549a27618f80686a68cda931da3e074a
Danny Gerst <d.gerst@bizrock.de>
2025-08-02 15:49:05 +0200

    Replaces default docusaurus Images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/mcs.jpg',
     navbar: {
       title: 'Model Context Standard',
       logo: {
